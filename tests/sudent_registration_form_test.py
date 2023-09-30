ab47de267b7e715506643100b14071f05dea86c6
Vitaliy Zakharov <zak125@mail.ru>
2023-09-30 21:18:17 +0300

    fixed up according mentor comments

--- tests/sudent_registration_form_test.py
+++ tests/sudent_registration_form_test.py
@@ -1,7 +1,5 @@
 from selene.support.shared import browser
-from selene import be, have
-
-import os
+from selene import have
 
 
 def test_submit_student_registration_form():
@@ -9,33 +7,22 @@ def test_submit_student_registration_form():
     browser.open('/automation-practice-form').wait_until(have.title('DEMOQA'))  # noqa
 
     # ACTIONS
-    browser.element('[id="firstName"]').should(have.attribute('placeholder').value('First Name')).type('Some')
-    browser.element('[id="lastName"]').should(have.attribute('placeholder').value('Last Name')).type('User')
-    browser.element('[id="userEmail"]').should(have.attribute('placeholder').value('name@example.com')).type(
-        'some@user.io')
+    browser.element('[id="firstName"]').type('Some')
+    browser.element('[id="lastName"]').type('User')
+    browser.element('[id="userEmail"]').type('some@user.io')
     browser.element('[for="gender-radio-1"]').click()
-    browser.element('[id="userNumber"]').should(have.attribute('placeholder').value('Mobile Number')).type(
-        '8800008800')
-
+    browser.element('[id="userNumber"]').type('8800008800')
     browser.element('[id="dateOfBirthInput"]').click()
     browser.element('[class="react-datepicker__year-select"]').click().element('option[value="1939"]').click()
     browser.element('[class="react-datepicker__month-select"]').click().element('option[value="9"]').click()
     browser.element('[class="react-datepicker__day react-datepicker__day--020"]').click()
-
-    browser.element('[id="subjectsInput"]').should(be.blank).click().type('maths').press_enter().type(
-        'hindi').press_enter()
+    browser.element('[id="subjectsInput"]').click().type('maths').press_enter().type('hindi').press_enter()
     browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('[for="hobbies-checkbox-2"]').click()
-
-    # browser.element('#uploadPicture').click().with_(timeout=browser.config.timeout * 2).send_keys(os.path.abspath('image/test.png'))
-    # browser.element('[for="uploadPicture"]').click().with_(timeout=browser.config.timeout * 2).send_keys(os.path.abspath('image/test.png'))
-
-    browser.element('[id="currentAddress"]').should(have.attribute('placeholder').value('Current Address')).type(
-        'Far far away')
-
+    browser.element('[class="form-control-file"]').send_keys('/home/sun/my_edu/qa_guru/g8_l5/image/test.png')
+    browser.element('[id="currentAddress"]').type('Far far away')
     browser.element('[id="react-select-3-input"]').type('rajasthan').press_enter()
     browser.element('[id="react-select-4-input"]').type('jaipur').press_enter()
-
     browser.element('[id="submit"]').click()
 
     # ASSERT
@@ -49,7 +36,8 @@ def test_submit_student_registration_form():
         '20 October,1939',
         'Maths, Hindi',
         'Sports, Reading',
-        '',
+        'test.png',
         'Far far away',
         'Rajasthan Jaipur'
     ))
+
